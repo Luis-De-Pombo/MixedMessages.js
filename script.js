2592818da6cc10a134f52a8203d5c7c180e249bd
Luis De Pombo <depombo2@gmail.com>
2023-05-11 15:40:53 -0500

    Implement selection and logging of phrase.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const subPhrases = {
-  sub1: [], 
-  sub2: [],
-  sub3: []
+  sub1: ["Go for an outdoor run", "Get a cup of morning coffee", "Watch the sunset"],
+  sub2: [" with your siblings", " with your date"],
+  sub3: [" on Saturday.", " on Sunday."]
 };
 
+const randEl = (arr) => {
+  let randI = Math.floor(Math.random() * arr.length);
+  return arr[randI];
+};
+
+let phrase = randEl(subPhrases.sub1) + randEl(subPhrases.sub2) + randEl(subPhrases.sub3);
+
+console.log(phrase);
